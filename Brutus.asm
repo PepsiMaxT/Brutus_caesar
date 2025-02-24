652f6da5f4f8cb4dc9b205f518da886ee80a7026
Max Taunton <maxtaunton@outlook.com>
2025-02-24 17:15:57 +0000

    Input getting in Brutus, removed executables from repo

--- Brutus.asm
+++ Brutus.asm
@@ -1,13 +1,26 @@
 section .note.GNU-stack noalloc noexec nowrite progbits
 
 section .data
-	testerInput db "Hello World!", 10, 0
-	testerShift dd 3	
+	msgGetEncrypted db "Enter the string you want to decrypt: ", 0
+
+section .bss
+	input resb 200
 
 section .text
         global main
         extern printf
 	extern caesar
+	extern fgets, stdin
 
 main:
+	push msgGetEncrypted	; Ask for input
+	call printf
+	add esp, 4		; Clean stack
+
+	push dword [stdin]	; Get input
+	push 200
+	push input
+	call fgets
+	add esp, 12		; Clean stack
+
 	ret
